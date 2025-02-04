ea088c083e350a018ac1044d4f83bd39f6d23f34
Squarepantspy <pgsantax@gmail.com>
2025-02-04 15:29:55 -0300

    sortedFriends

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let friends = [];
+let htmlist = '';
+let listado = document.getElementById('listaAmigos');
+let input = document.getElementById('amigo');
+let sorted = document.getElementById('resultado');
+const agregarAmigo = ()=>{
+    let friend = input.value;
+    if(friend){
+        friends.push(friend);
+        input.value = '';
+        actualizarAmigos();
+    }else{
+        alert('Debe ingresar un nombre');
+    }
+    
+}
+const actualizarAmigos = ()=>{
+    for (let i=0; i<friends.length; i++){
+        htmlist += `<li>${friends[i]}</li>`
+    }
+    listado.innerHTML = htmlist;
+    htmlist='';
+}
+const sortearAmigo=()=>{
+    let random = Math.floor(Math.random()*friends.length);
+    sorted.innerHTML = `<li>El amigo sorteado es ${friends[random]}</li>`
+}
